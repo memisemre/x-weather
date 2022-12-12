d7aef1c411d604a895fa157da919e3e8d2147de7
memisemre <dev.emrememis@gmail.com>
2022-12-12 14:22:09 +0300

    Edit Api Functions

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-import { loginLoaderAnimation } from "./js/loginLoaderAnimation.js";
 import { weatherApi } from "./js/weatherForecast.js";
 function startApp() {
 
-          weatherApi()
-};
+          weatherApi();
+}
 startApp();
--- js/weatherForecast.js
+++ js/weatherForecast.js
@@ -1,12 +1,11 @@
 export function weatherApi(){
          if(navigator.geolocation){
-             navigator.geolocation.getCurrentPosition((position)=>{
-                  console.log(position.coords.latitude + " " + position.coords.longitude)
-      let data = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.longitude}&lon=${position.coords.latitude}&appid=b923a9422672988586e6e7f2bcef29c9&units=metric&lang=en`)
-      .then((res)=>res.json())
-     .then((data)=>{
-               console.log(data);
-          })
-        })
-       }
+                   navigator.geolocation.getCurrentPosition((position) => {
+                             let data = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.longitude}&lon=${position.coords.latitude}&appid=b923a9422672988586e6e7f2bcef29c9&units=metric&lang=en`)
+                                       .then((res) => res.json())
+                                       .then((data) => {
+                                                 console.log(data);
+                                       });
+                   });
+          }
  }
