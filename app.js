53be4aab190a0f70fca11f3a70627b5b8c98b22d
Emre Memiş <113227617+memisemre@users.noreply.github.com>
2022-12-13 21:04:21 +0300

    [JS Function] AlertBox and CityApi  Operations

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import { getDate } from "./js/getUserDate.js";
 import { getLocation } from "./js/getUserLocation.js";
 import { loginAnimation } from "./js/loginAnimation.js"; 
-import { cityNameArea,cityHoverText,changeCityContainer,appContainer, changeCityButton, changeCityInput } from "./js/selectors.js";
+import { alertBox } from "./js/alertPopup.js"
+import { weatherCityAPI } from "./js/weatherApi.js";
+import { cityNameArea,cityHoverText,changeCityContainer,appContainer, changeCityButton, changeCityInput, loginAnimationContainer} from "./js/selectors.js";
 function startApp() {
         getDate();
         getLocation();
@@ -23,6 +25,14 @@ changeCityInput.addEventListener("click",()=>{
 })
 changeCityButton.addEventListener("click",()=>{
         if(changeCityInput.value === "Enter city name." || changeCityInput.value === ""){
-                alert("error");
+                alertBox("Please,enter city names.")
         }
+        else {
+                weatherCityAPI(changeCityInput.value);
+                changeCityContainer.style.display = "none";
+                loginAnimationContainer.style.display = "flex";
+                loginAnimationContainer.style.opacity = "1";
+                loginAnimation();
+        }
+
 })
--- js/weatherApi.js
+++ js/weatherApi.js
@@ -6,6 +6,8 @@ import{cityArea,
 	minTemp,
 	wind
 } from "./selectors.js"
+import { alertBox } from "./alertPopup.js"
+
 export function weatherAPI(lat,long){
       let data = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=b923a9422672988586e6e7f2bcef29c9&units=metric&lang=en`)
 	.then((res)=>res.json())
@@ -17,6 +19,23 @@ export function weatherAPI(lat,long){
 		maxTemp.innerHTML = `${data.main.temp_max}<sup>0</sup>`;
 		minTemp.innerHTML = `${data.main.temp_min}<sup>0</sup>`;
 		wind.innerHTML = `${data.wind.speed}<sup></sup>`;
+		console.log(data)
+	})
+}
+export function weatherCityAPI(city){
+	let data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b923a9422672988586e6e7f2bcef29c9&units=metric&lang=en`)
+	.then((res)=>res.json())
+	.then((data)=>{
+		if(data.cod === "404")alertBox("Please, check city name. Don't use the turkish characters.") 
+		else{
+			cityArea.innerHTML = `${data.name}`;
+			weatherImage.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">`;
+			weatherTemp.innerHTML = `${data.main.temp}<sup>0</sup>`;
+			weatherForecast.innerHTML = `${data.weather[0].main}`;
+			maxTemp.innerHTML = `${data.main.temp_max}<sup>0</sup>`;
+			minTemp.innerHTML = `${data.main.temp_min}<sup>0</sup>`;
+			wind.innerHTML = `${data.wind.speed}<sup></sup>`;
+		}
 	})
 }
 // const temporaryData = fetch("https://api.openweathermap.org/data/2.5/weather?q=ankara&appid=b923a9422672988586e6e7f2bcef29c9&units=metric&lang=en")
